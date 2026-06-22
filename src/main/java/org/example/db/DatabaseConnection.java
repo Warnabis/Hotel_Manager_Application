d41d2e48122863dac603e8a55aca90b9306fb7c3
Trifonko Vyacheslav 350502 <vtrifonko@gmail.com>
2026-06-22 09:43:21 +0300

    Fixing Sonar RuntimeException

--- src/main/java/org/example/db/DatabaseConnection.java
+++ src/main/java/org/example/db/DatabaseConnection.java
@@ -14,13 +14,13 @@ public class DatabaseConnection {
         try (InputStream input = DatabaseConnection.class.getClassLoader()
           .getResourceAsStream("dbconfig.properties")) {
             if (input == null) {
-                throw new RuntimeException("Файл dbconfig.properties не найден в classpath");
+                throw new IllegalStateException("Файл dbconfig.properties не найден в classpath");
             }
             props.load(input);
 
             Class.forName(props.getProperty("db.driver"));
         } catch (Exception e) {
-            throw new RuntimeException("Ошибка загрузки конфигурации БД", e);
+            throw new IllegalStateException("Ошибка загрузки конфигурации БД", e);
         }
     }
 
