1875e83a26ae0119fa1f193ac36116e00f8bac0f
Trifonko Vyacheslav 350502 <vtrifonko@gmail.com>
2026-06-23 11:14:29 +0300

    Fixing package register

--- backend/src/main/java/com/warnabis/Hotel_SpringBoot_Application/exception/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.warnabis.Hotel_SpringBoot_Application.exception;
-
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/warnabis/Hotel_SpringBoot_Application/exception/ConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.warnabis.Hotel_SpringBoot_Application.exception;
-
-public class ConflictException extends RuntimeException {
-
-    public ConflictException(String message) {
-        super(message);
-    }
-}
