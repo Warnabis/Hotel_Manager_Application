6625cbbaec7a2e2339259da8d31e1d61f358a8b2
Trifonko Vyacheslav 350502 <vtrifonko@gmail.com>
2026-06-25 09:51:25 +0300

    Delete backend/main directory
    Odd directory

--- backend/main/java/com/warnabis/hotel_springboot_application/exception/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.warnabis.hotel_springboot_application.exception;
-
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- backend/main/java/com/warnabis/hotel_springboot_application/exception/ConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.warnabis.hotel_springboot_application.exception;
-
-public class ConflictException extends RuntimeException {
-
-    public ConflictException(String message) {
-        super(message);
-    }
-}
